b4ad7f56cacb15662670a1c78bc893d16bc5eedf
Pavan-5-5-5 <155568268+Pavan-5-5-5@users.noreply.github.com>
2024-01-27 16:31:54 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,3 +1,7 @@
+
+
+
+
 function calculate() {
     var num1 = parseFloat(document.getElementById('num1').value);
     var operator = document.getElementById('operator').value;
@@ -10,22 +14,70 @@ function calculate() {
         document.getElementById('answer').style.fontSize = '1.5rem' ;
     }
     else{
-    if (operator == '+') {
-        answer = num1 + num2;
-    } else if (operator == '-') {
-        answer = num1 - num2;
-    }
-     else if (operator == 'x') {
-        answer = num1 * num2;
-    }
-     else if (operator == '/') {
-        answer = num1 / num2;
-    }
-     else if (operator == '^') {
-        answer = Math.pow(num1, num2);
-    } 
+        document.getElementById('answer').style.color = 'black';
+
+        if (operator === '+') {
+            answer = num1 + num2;
+        } else if (operator === '-') {
+            answer = num1 - num2;
+        } else if (operator == '/') {
+            answer = num1 / num2;
+        } else if (operator === 'x') {
+            answer = num1 * num2;
+        } else if (operator === '^') {
+            answer = Math.pow(num1, num2);
+        }
+        
      
 
     document.getElementById('answer').innerText = '= ' + answer;
    }
 } 
+
+
+// this is for uhh , the down calculator button part , OVER
+
+const writer = document.getElementById('writer');
+
+   
+const buttons = document.querySelectorAll('.grid-item:not(#undo)');
+buttons.forEach(button => {
+    button.addEventListener("click", function() {
+        
+        writer.innerText += button.value;
+    });
+ });
+
+const undo = document.getElementById('undo');
+undo.addEventListener("click", function() {
+       if (writer.innerText== "Invalid Expression") {
+          writer.innerText= " "
+          document.getElementById('writer').style.color = 'black';
+          document.getElementById('writer').style.backgroundColor = 'rgba(255, 255, 255, 0.5)';
+       } else {
+        
+       
+        let before = writer.innerText;
+        let after = before.slice(0, -1);
+        writer.innerText = after;
+       }
+});
+
+
+function updateResult() {
+    try {
+        const result = eval(writer.innerText);
+        document.getElementById('writer').innerText = '= ' + result;
+    } catch (error) {
+        document.getElementById('writer').innerText = 'Invalid Expression';
+        document.getElementById('writer').style.color = 'red';
+        document.getElementById('writer').style.backgroundColor = 'transparent';
+    }
+}
+
+
+const clearBtn = document.getElementById('clearBtn');
+clearBtn.addEventListener('click', function () {
+    let writer = document.getElementById('writer');
+    writer.innerText = " "
+})
